ad49ef6f8ef0804298d2af02a2ac25074711d6aa
Anna Royer <anna.royer@colorado.edu>
2018-07-11 13:28:33 -0600

    add test for summary method

--- test/dog_test.rb
+++ test/dog_test.rb
@@ -10,4 +10,10 @@ class DogTest < Minitest::Test
 
     assert_instance_of Dog, dog
   end
+
+  def test_it_prints_a_summary
+    dog = Dog.new("Fido", 2, "Dalmation")
+
+    assert_equal "Name: Fido\nAge: 2\nBreed: Dalmation", dog.summary
+  end
 end
